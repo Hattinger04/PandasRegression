215f95d3c203f813efba5dd051742aefddb67ea2
Hattinger04 <s.8greiderer@gmail.com>
2022-12-15 09:13:44 +0100

    finish

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 import statsmodels.api as sm
 
 df = pd.read_csv('bev_meld.csv', sep=";")
-
+print(df)
 # Tirolweit:
 df_sum = df.sum()[3:]
 print(df_sum)
@@ -53,4 +53,19 @@ plt.xlim([2000, 2100])
 plt.show()
 print(model.params)
 
-#
+fig, axes = plt.subplots(2,1)
+t1 = df.values[1:24, 3:].sum(axis=0)
+d1 = pd.DataFrame({"Bev": t1})
+t2 = df.values[140:169, 3:].sum(axis=0)
+d2 = pd.DataFrame({"Bev": t2})
+
+d1.plot(ax=axes[0], title="Innsbruck Land")
+d2.plot(ax=axes[1], title="Landeck")
+
+fig.tight_layout()
+plt.show()
+
+plt.plot(df.columns[3:], d1, label="IL")
+plt.plot(df.columns[3:], d2, label="L")
+plt.legend()
+plt.show()
